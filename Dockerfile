cc21370f9d98197b27fc273a922838b2e02d7852
josebetancourtzam18 <josebetancourtzam18@gmail.com>
2023-04-20 16:19:26 -0400

    Re-ajuste de tamaño de firma y cambio de estilos

---
+++ Dockerfile
@@ -0,0 +1,73 @@
+FROM node:18.1.0-alpine3.14 AS installer
+
+RUN apk add --no-cache libc6-compat
+
+RUN apk add vim -v --progress
+
+RUN apk add nano -v --progress
+
+WORKDIR /home/node/app
+
+COPY package.json ./
+
+RUN yarn install
+
+# STAGE 2 APP BUILDING PROCESS
+
+FROM node:18.1.0-alpine3.14  AS builder
+
+RUN apk add --no-cache libc6-compat
+
+RUN apk add vim -v --progress
+
+RUN apk add nano -v --progress
+
+WORKDIR /home/node/app
+
+RUN mkdir node_modules
+
+COPY . .
+
+COPY --from=installer /home/node/app/node_modules ./node_modules
+
+COPY --from=installer /home/node/app/yarn.lock .
+
+ENV GENERATE_SOURCEMAP=false
+
+ENV NODE_OPTIONS=openssl-legacy-provider
+
+ENV PUBLIC_URL=/digitalSignature
+
+RUN yarn build
+
+# STAGE 3 DESPLIEGUE DE LA APLICACION EN SERVIDOR NGINX
+
+FROM nginx:1.21.6-alpine as deployer
+
+ENV PUBLIC_URL=/digitalSignature
+
+RUN apk add --no-cache libc6-compat
+
+RUN apk add vim -v --progress
+
+RUN apk add nano -v --progress
+
+WORKDIR /usr/share/nginx/html
+
+RUN rm -rf ./*
+
+RUN mkdir ./digitalSignature
+
+RUN chown -R nginx:nginx ./
+
+COPY default.conf /etc/nginx/conf.d
+
+COPY --from=builder --chown=nginx:nginx /home/node/app/build /usr/share/nginx/html/digitalSignature
+
+RUN touch /var/run/nginx.pid && chown nginx:nginx /var/run/nginx.pid  && chown -R nginx:nginx /var/cache/nginx/
+
+EXPOSE 9001
+
+USER nginx
+
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
--- src/App.js
+++ src/App.js
@@ -1,63 +1,100 @@
-import React, { useState, useEffect, useRef } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SignatureCanvas from 'react-signature-canvas';
-
-import { Navbar } from '../src/Navbar/Navbar';
-import { PiramideBrand } from '../src/Brands/PiramideBrand/PiramideBrand';
-import { OceanicaBrand } from '../src/Brands/OceanicaBrand/OceanicaBrand';
+import React, { useState, useEffect, useRef } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import SignatureCanvas from "react-signature-canvas";
+import Button from "@mui/material/Button";
+import { Navbar } from "../src/Navbar/Navbar";
+import { PiramideBrand } from "../src/Brands/PiramideBrand/PiramideBrand";
+import Logo from './Image/icon_piramide.png'
+// import { OceanicaBrand } from '../src/Brands/OceanicaBrand/OceanicaBrand';
 
 function App() {
   const signatureRef = useRef();
   const [firma, setFirma] = useState(null);
-
-  useEffect(() => {
-    setFirma(signatureRef.current.toDataURL());
-  }, []);
 
   const handleClear = () => {
     signatureRef.current.clear();
   };
 
   const handleSave = () => {
-    const signature = signatureRef.current.toDataURL();
+    const signature = signatureRef.current.getTrimmedCanvas().toDataURL();
     console.log(signature);
     if (signature != firma) {
-      alert('Firma guardada correctamente');
+      alert("Firma guardada correctamente");
     } else {
-      alert('Por favor Firme antes de guardar');
+      alert("Por favor Firme antes de guardar");
     }
   };
 
+  useEffect(() => {
+    setFirma(signatureRef.current.toDataURL());
+  }, []);
+
   return (
     <>
-      <Navbar
-        brand={
-          // CONFIG.environment.company === 'PIRAMIDE' ? (
-          <PiramideBrand width="85%" height="85%" />
-          // ) : (
-          //   <OceanicaBrand width="100%" height="100%" />
-          // )
-        }
-      />
+      <Navbar brand={<PiramideBrand width="85%" height="85%" />} />
       <div
         style={{
-          backgroundColor: 'white',
-          borderRadius: '5px',
+          backgroundColor: "white",
+          borderRadius: "5px",
         }}
         className="container-signature"
       >
-        <SignatureCanvas
-          ref={signatureRef}
-          className="sigCanvas"
-          penColor="black"
-          canvasProps={{
-            className: 'sigCanvas',
+        <div
+          style={{
+            border: "2px solid red",
+            width: "100%",
+            height: "100%",
+            display: "grid",
+            placeContent: "center",
           }}
-        />
-        <div>
-          <button onClick={handleClear}>Borrar firma</button>
-          <button onClick={handleSave}>Guardar firma</button>
+        >
+          <div
+            style={{
+              width: "500px",
+              height: "22rem",
+              display: "grid",
+              placeContent: "center",
+              borderRadius: "1rem",
+              background: "white",
+            }}
+          >
+            <div className="container-logo">
+                <img src={Logo} className="logo"/>
+              </div>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  border: "2px solid #c4c7cf",
+                  width: "350px",
+                  height: "150px",
+                  background: "white",
+                  display: "grid",
+                  placeItems: "center",
+                  borderRadius: '1rem'
+                }}
+              >
+                <SignatureCanvas
+                  ref={signatureRef}
+                  className="sigCanvas"
+                  penColor="black"
+                  canvasProps={{
+                    className: "sigCanvas",
+                  }}
+                />
+              </div>
+            </div>
+            <div style={{marginTop: '2rem'}}>
+              <button onClick={handleClear}>Borrar firma</button>
+              <button onClick={handleSave}>Guardar firma</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
