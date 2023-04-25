69964a8be533085c278b51d036e401e09c8d2f18
Reinaldo Requena <reinaldo.r.anincubator@gmail.com>
2023-04-25 16:46:50 +0200

    fix funcion para descargar firma

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 // import logo from './logo.svg';
-import "./App.css";
-import SignatureCanvas from "react-signature-canvas";
-import Button from "@mui/material/Button";
-import { Navbar } from "../src/Navbar/Navbar";
-import { PiramideBrand } from "../src/Brands/PiramideBrand/PiramideBrand";
-import Logo from './Image/icon_piramide.png'
+import './App.css';
+import SignatureCanvas from 'react-signature-canvas';
+import Button from '@mui/material/Button';
+import { Navbar } from '../src/Navbar/Navbar';
+import { PiramideBrand } from '../src/Brands/PiramideBrand/PiramideBrand';
+import Logo from './Image/icon_piramide.png';
 // import { OceanicaBrand } from '../src/Brands/OceanicaBrand/OceanicaBrand';
 
 function App() {
@@ -19,11 +19,10 @@ function App() {
   const handleSave = () => {
     const signature = signatureRef.current.getTrimmedCanvas().toDataURL();
     console.log(signature);
-    if (signature != firma) {
-      alert("Firma guardada correctamente");
-    } else {
-      alert("Por favor Firme antes de guardar");
-    }
+    const downloadLink = document.createElement('a');
+    downloadLink.href = signature;
+    downloadLink.download = 'image.png';
+    downloadLink.click();
   };
 
   useEffect(() => {
@@ -35,32 +34,24 @@ function App() {
       <Navbar brand={<PiramideBrand width="85%" height="85%" />} />
       <div
         style={{
-          backgroundColor: "white",
-          borderRadius: "5px",
+          backgroundColor: 'white',
+          borderRadius: '5px',
         }}
         className="container-signature"
       >
-        <div
-         className="container-card"
-        >
-          <div
-          className="card"
-          >
+        <div className="container-card">
+          <div className="card">
             <div className="container-logo">
-                <img src={Logo} className="logo"/>
-              </div>
-            <div
-             className="container-signature-limit"
-            >
-              <div
-                className="container-sigCanvas"
-              >
+              <img src={Logo} className="logo" />
+            </div>
+            <div className="container-signature-limit">
+              <div className="container-sigCanvas">
                 <SignatureCanvas
                   ref={signatureRef}
                   className="sigCanvas"
                   penColor="black"
                   canvasProps={{
-                    className: "sigCanvas",
+                    className: 'sigCanvas',
                   }}
                 />
               </div>
