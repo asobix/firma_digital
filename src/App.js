e168dc54a2f405f0a4a5b489fb7200d43cbd6420
josebetancourtzam18 <josebetancourtzam18@gmail.com>
2023-04-25 10:48:45 -0400

    trabajando con oceanica

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { useState, useEffect, useRef } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import SignatureCanvas from "react-signature-canvas";
-import Button from "@mui/material/Button";
 import { Navbar } from "../src/Navbar/Navbar";
 import { PiramideBrand } from "../src/Brands/PiramideBrand/PiramideBrand";
-import Logo from './Image/icon_piramide.png'
-// import { OceanicaBrand } from '../src/Brands/OceanicaBrand/OceanicaBrand';
+import LogoPiramide from './Image/icon_piramide.png'
+import LogoOceanica from './Image/icon_oceanica.png'
+import { OceanicaBrand } from '../src/Brands/OceanicaBrand/OceanicaBrand';
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background: ${process.env.REACT_APP_COMPANY !== 'OCEANICA' ? 'red' : '#4abfaf'} ;
+    color: white;
+    border: none;
+    padding: 15px;
+    text-transform: capitalize;
+    border-radius: 5px;
+    margin: 10px;
+    box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
+    cursor: pointer;
+`;
 
 function App() {
   const signatureRef = useRef();
@@ -18,7 +31,10 @@ function App() {
 
   const handleSave = () => {
     const signature = signatureRef.current.getTrimmedCanvas().toDataURL();
-    console.log(signature);
+    const img = new Image()
+    img.src = signature
+    document.body.appendChild(img);
+    console.log(img);
     if (signature != firma) {
       alert("Firma guardada correctamente");
     } else {
@@ -32,13 +48,9 @@ function App() {
 
   return (
     <>
-      <Navbar brand={<PiramideBrand width="85%" height="85%" />} />
+      <Navbar brand={process.env.REACT_APP_COMPANY !== 'OCEANICA' ? <PiramideBrand width="85%" height="85%" /> : <OceanicaBrand width="85%" height="85%" />} />
       <div
-        style={{
-          backgroundColor: "white",
-          borderRadius: "5px",
-        }}
-        className="container-signature"
+        className={process.env.REACT_APP_COMPANY !== 'OCEANICA' ? "container-signature" : 'container-signature-oceanica'}
       >
         <div
          className="container-card"
@@ -47,7 +59,7 @@ function App() {
           className="card"
           >
             <div className="container-logo">
-                <img src={Logo} className="logo"/>
+                <img src={process.env.REACT_APP_COMPANY !== 'OCEANICA' ? LogoPiramide : LogoOceanica} className="logo"/>
               </div>
             <div
              className="container-signature-limit"
@@ -66,8 +78,8 @@ function App() {
               </div>
             </div>
             <div className="container-button">
-              <button onClick={handleClear}>Borrar firma</button>
-              <button onClick={handleSave}>Guardar firma</button>
+              <Button onClick={handleClear}>Borrar firma</Button>
+              <Button onClick={handleSave}>Guardar firma</Button>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HelmetProvider, Helmet as Head } from "react-helmet-async";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
+    <Head>
+          <link rel="icon" href={`${process.env.PUBLIC_URL}${process.env.REACT_APP_UTL_HTML_FAVICON}`} />
+          <title>{process.env.REACT_APP_HTML_TITLE}</title>
+        </Head>
     <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
